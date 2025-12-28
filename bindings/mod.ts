734b0b4c013059e328cd86c482de850dc1d19269
Adam Due Hansen <adamduehan@gmail.com>
2025-12-28 22:48:54 +0100

    bindings: moves event-emitter and resource to engine.

--- bindings/mod.ts
+++ bindings/mod.ts
@@ -1,10 +1,7 @@
-export * as "audio" from "./src/core/r-audio.ts";
-export * as "core" from "./src/core/r-core.ts";
-export * as "math" from "./src/core/r-math.ts";
-export * as "models" from "./src/core/r-models.ts";
-export * as "shapes" from "./src/core/r-shapes.ts";
-export * as "text" from "./src/core/r-text.ts";
-export * as "textures" from "./src/core/r-textures.ts";
-
-export * from "./src/resource.ts";
-export * from "./src/event-emitter.ts";
+export * as "audio" from "./src/r-audio.ts";
+export * as "core" from "./src/r-core.ts";
+export * as "math" from "./src/r-math.ts";
+export * as "models" from "./src/r-models.ts";
+export * as "shapes" from "./src/r-shapes.ts";
+export * as "text" from "./src/r-text.ts";
+export * as "textures" from "./src/r-textures.ts";
