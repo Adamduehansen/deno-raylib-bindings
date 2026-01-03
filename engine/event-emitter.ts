243bdcae72661452b27af852ca1d2d8ce441de53
Adam Due Hansen <adamduehan@gmail.com>
2026-01-03 22:28:39 +0100

    A bit of cleanup of event-emitter.

--- engine/event-emitter.ts
+++ engine/event-emitter.ts
@@ -1,45 +1,47 @@
-type EventHandler<TEventData> = (data?: TEventData) => void;
+type EventMap = Record<string, unknown>;
+type EventKey<T extends EventMap> = string & keyof T;
+type EventHandler<EventKey> = (data?: EventKey) => void;
 
 export class EventEmitter<
-  TEventMap extends Record<string, unknown> = Record<string, unknown>,
+  TEventMap extends EventMap = Record<string, unknown>,
 > {
   private readonly _eventMap = new Map<
     keyof TEventMap,
     EventHandler<unknown>[]
   >();
 
-  emit<TEvent extends keyof TEventMap>(
-    event: TEvent,
-    data?: TEventMap[TEvent],
+  emit<TEventName extends EventKey<TEventMap>>(
+    event: TEventName,
+    data?: TEventMap[TEventName],
   ): void;
   emit(event: string, data?: unknown): void;
-  emit<TEvent extends keyof TEventMap>(
+  emit<TEvent extends EventKey<TEventMap>>(
     event: TEvent | string,
     data?: TEventMap[TEvent] | unknown,
   ) {
-    const handlers = this._eventMap.get(event as keyof TEventMap);
+    const handlers = this._eventMap.get(event);
     if (!handlers) {
       return;
     }
 
     for (const handler of handlers) {
-      (handler as EventHandler<TEventMap[TEvent]>)(data as any);
+      handler(data);
     }
   }
 
-  on<TEvent extends keyof TEventMap>(
+  on<TEvent extends EventKey<TEventMap>>(
     event: TEvent,
     handler: EventHandler<TEventMap[TEvent]>,
   ): void;
   on(event: string, handler: EventHandler<unknown>): void;
-  on<TEvent extends keyof TEventMap>(
+  on<TEvent extends EventKey<TEventMap>>(
     event: TEvent | string,
     handler: EventHandler<TEventMap[TEvent]> | EventHandler<unknown>,
   ) {
-    let handlers = this._eventMap.get(event as keyof TEventMap);
+    let handlers = this._eventMap.get(event);
     if (handlers === undefined) {
       handlers = [];
-      this._eventMap.set(event as keyof TEventMap, handlers);
+      this._eventMap.set(event, handlers);
     }
 
     handlers.push(handler as EventHandler<unknown>);
