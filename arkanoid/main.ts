2577594cc95bf9284905be287b941a70630ad739
Adam Due Hansen <adamduehan@gmail.com>
2026-01-21 06:16:16 +0100

    arkanoid: adds all levels.

--- arkanoid/main.ts
+++ arkanoid/main.ts
@@ -4,6 +4,7 @@ import StartScene from "./scenes/start-scene/start-scene.ts";
 import { GameOverScene } from "./scenes/game-over-scene.ts";
 import Level1Scene from "./scenes/level-scene/level-1-scene.ts";
 import Level2Scene from "./scenes/level-scene/level-2-scene.ts";
+import Level3Scene from "./scenes/level-scene/level-3-scene.ts";
 
 const SCREEN_WIDTH = 800;
 const SCREEN_HEIGHT = 450;
@@ -17,6 +18,7 @@ const game = new Game({
     "start": new StartScene(),
     "level-1": new Level1Scene(),
     "level-2": new Level2Scene(),
+    "level-3": new Level3Scene(),
     "game-over": new GameOverScene(),
   },
   background: RayWhite,
