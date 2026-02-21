b2e97d19b51fee0e0305f8237ab14ff1e93b0a0c
Adam Due Hansen <adamduehan@gmail.com>
2026-02-21 08:24:43 +0100

    dungeon-heroes: adds tests for component-manager.

--- dungeon-heroes/component-manager.test.ts
+++ dungeon-heroes/component-manager.test.ts
@@ -1,1 +1,34 @@
-// TODO
+import { assertEquals, assertFalse } from "@std/assert";
+import { Entity } from "./entity.ts";
+import ComponentManager from "./component-manager.ts";
+import { Component } from "./components.ts";
+
+Deno.test("should add component for entity", () => {
+  // Arrange
+  class TestComponent implements Component {}
+  const entity = new Entity();
+  const componentManager = new ComponentManager();
+
+  // Act
+  const testComponent = new TestComponent();
+  componentManager.add(entity, testComponent);
+
+  // Assert
+  assertEquals(componentManager.has(entity, TestComponent), true);
+  assertEquals(componentManager.get(entity, TestComponent), testComponent);
+});
+
+Deno.test("should return null for component that does not exist", () => {
+  // Arrange
+  class TestComponent implements Component {}
+  const entity = new Entity();
+  const componentManager = new ComponentManager();
+
+  // Act
+  const hasComponent = componentManager.has(entity, TestComponent);
+  const testComponent = componentManager.get(entity, TestComponent);
+
+  // Assert
+  assertFalse(hasComponent);
+  assertEquals(testComponent, null);
+});
