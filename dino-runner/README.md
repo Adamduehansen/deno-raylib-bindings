c89b81f259f1b5a173431cadebdda2119a147299
Adam Due Hansen <adamduehan@gmail.com>
2026-02-24 22:15:36 +0100

    Removes noise.

--- dino-runner/README.md
+++
@@ -1,5 +0,0 @@
-# Dino runner
-
-In december 2025 the Deno team published a blog series on building a dino
-runner. The blog builds for the web - this is a interpretation of the game but
-build with the Raylib bindings.
--- engine/mod.ts
+++
@@ -1,7 +0,0 @@
-export * from "./entity.ts";
-export * from "./vector.ts";
-export * from "./physics.ts";
-export * from "./scene.ts";
-export * from "./event-emitter.ts";
-export * from "./resource.ts";
-export * from "./game.ts";
