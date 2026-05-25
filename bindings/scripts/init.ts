27597dc00f642c14bfc76e5257b8f7cf58d09af6
Adam Due Hansen <adamduehan@gmail.com>
2026-05-25 14:38:26 +0200

    Init script now creates libs.

--- bindings/scripts/init.ts
+++ bindings/scripts/init.ts
@@ -138,6 +138,11 @@ for await (
   );
 }
 
+await Deno.copyFile(
+  resolve(tmpFolder, "./raylib-5.5_linux_amd64/lib/libraylib.so.5.5.0"),
+  resolve(projectDir, "lib/libraylib.so.5.5.0"),
+);
+
 await Deno.remove(tmpFolder, { recursive: true });
 
 /**
